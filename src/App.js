ec9e28e02d5f08ef6ea0d2fbb4278b5a7ee79b00
Emon1201 <faisal.emon1201@gmail.com>
2025-08-29 03:45:23 +0600

    Polished and added some functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
   constructor() {
-    super(); 
+    super() 
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
-    };
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => { this.setState({ robots: users }) });
   }
 
   onSearchChange = (event) => {
@@ -20,16 +26,23 @@ class App extends Component {
   render() {
     const filterRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    });
+    })
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
 
     return (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filterRobots} />
+        <Scroll>
+          <CardList robots={filterRobots} />
+        </Scroll>
+        
       </div>
     );
   } 
+}
 }
 
 export default App;
