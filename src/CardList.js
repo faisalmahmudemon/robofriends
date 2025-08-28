de6d831711d50d140a9a294a9829f89cca1f9489
Emon1201 <faisal.emon1201@gmail.com>
2025-08-29 01:05:05 +0600

    updated files and styles

--- src/CardList.js
+++ src/CardList.js
@@ -4,7 +4,7 @@ import Card from './Card';
 
 const CardList = ({robots}) => {
     return (
-        <div className= 'tc'>  
+        <div>  
             {
                 robots.map((user, i) => {
                     return (
